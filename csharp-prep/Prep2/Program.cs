7c19a278e386da7daf7a2d91e25cec0bebd44f74
Eric Flamm <ericwflamm@gmail.com>
2026-01-09 15:33:06 -0700

    C# prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+        Console.WriteLine("What is your grade (100-0) ");
+        string user_grade = Console.ReadLine();
+        int grade = int.Parse(user_grade);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congrats You Passed The Class with a {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry you failed the class, you got it next time");
+        }
+
     }
 }
