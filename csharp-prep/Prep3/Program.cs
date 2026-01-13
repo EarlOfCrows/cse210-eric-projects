c3c252978f7f22c731f078daae35b6624668f547
Eric Flamm <ericwflamm@gmail.com>
2026-01-13 09:43:45 -0700

    lesson 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.IO.Pipelines;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magic_number = 27;
+
+        do 
+        { 
+            Console.WriteLine($"What is the Magic number: {magic_number}");
+            Console.Write("What is your guess: ");
+            string guess = Console.ReadLine();
+            int num_guess = int.Parse(guess);
+            if (num_guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (num_guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+        } while (true);
+
     }
 }
