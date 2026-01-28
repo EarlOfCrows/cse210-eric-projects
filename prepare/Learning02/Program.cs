16322b9b0c82b396fc27d018cb1e65016908ab7e
Eric Flamm <ericwflamm@gmail.com>
2026-01-28 14:46:53 -0700

    learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2026;
+        job1._endYear = 2029;
+        Job job2 = new Job();
+        job2._jobTitle = "Game Designer";
+        job2._company = "Nintendo";
+        job2._startYear = 2025;
+        job2._endYear = 2029;
+        Job job3 = new Job();
+        job3._jobTitle = "Junior Developer";
+        job3._company = "Microsoft";
+        job3._startYear = 2027;
+        job3._endYear = 2030;
+        job1.Display();
+        job2.Display();
+        job3.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "greg";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1._jobs.Add(job3);
+        resume1.Display();
+
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs;
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}\nJobs:\n{_jobs[0]._jobTitle}");
+    }
+}
